db858139ee1e997896e805fc349c7a003803b5a2
tastycrayon <mosiur.404.rahman@gmail.com>
2023-05-07 09:20:08 +0600

    -

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,20 @@
-import adapter from "@sveltejs/adapter-auto";
-// import { vitePreprocess } from "@sveltejs/kit/vite";
-import preprocess from "svelte-preprocess";
+import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+// import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
-  // preprocess: vitePreprocess(),
-  preprocess: preprocess(),
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+	// preprocess: preprocess(),
 
-  kit: {
-    adapter: adapter(),
-  },
-  optimizeDeps: {
-    exclude: ["@urql/svelte"],
-  },
+	kit: {
+		adapter: adapter()
+	},
+	optimizeDeps: {
+		exclude: ['@urql/svelte']
+	}
 };
 
 export default config;
